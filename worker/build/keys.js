491690db5601f29ddabab06853eeef7e54418e57
KANSO Abdallah <abdallah.kanso@unine.ch>
2020-09-14 22:08:39 +0200

    fix scripts in package.json for worker and server

--- worker/build/keys.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.redisPort = exports.redisHost = void 0;
-exports.redisHost = process.env.REDIS_HOST;
-exports.redisPort = process.env.REDIS_PORT;
